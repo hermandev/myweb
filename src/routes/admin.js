8ffd0e814c9a6e24ec887df11abb8ffa3f39ef01
hermandev <hermantolakoro@gmail.com>
2019-09-09 18:06:38 +0800

    add middleware

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,62 +1,82 @@
 const express = require('express')
 const router = express.Router()
+// Database
+const Users = require('../models/Users.js')
 
 
 
 // DASHBOARD
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   res.render('pages/dashboard/index')
 })
 // END DASHBOARD
 
 
 // POSTS
-router.get('/posts', (req, res) => {
+router.get('/posts', async (req, res) => {
   res.render('pages/dashboard/posts')
 })
 // END POSTS
 
 
 // GALERY
-router.get('/galery', (req, res) => {
+router.get('/galery', async (req, res) => {
   res.render('pages/dashboard/galery')
 })
 // END GALERY
 
 
 // ANALITYCS
-router.get('/analitycs', (req, res) => {
+router.get('/analitycs', async (req, res) => {
   res.render('pages/dashboard/analitycs')
 })
 // END ANALITYCS
 
 
 // SHOP
-router.get('/shop', (req, res) => {
+router.get('/shop', async (req, res) => {
   res.render('pages/dashboard/shop')
 })
 // END SHOP
 
 
 // COMMENTS
-router.get('/comments', (req, res) => {
+router.get('/comments', async (req, res) => {
   res.render('pages/dashboard/comments')
 })
 // END COMMENTS
 
 
 // SETTINGS
-router.get('/settings', (req, res) => {
+router.get('/settings', async (req, res) => {
   res.render('pages/dashboard/settings')
 })
 // END SETTINGS
 
+
+// REGISTER
+router.get('/user/register', async (req, res) => {
+  res.render('pages/dashboard/register')
+})
+
+router.post('/user/register', async (req, res) => {
+  const data = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  }
+  res.render('pages/dashboard/register',{data: data})
+})
+
+// END REGISTER
+
+
 // LOGIN
-router.get('/login', (req, res) => {
+router.get('/login', async (req, res) => {
   res.render('pages/dashboard/login')
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   res.render('pages/dashboard/login')
   res.json({
   	username: req.body.username,
